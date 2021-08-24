31b47a776b1969b48541506ece1a4c7d792a7bef
Vladimir Komarovski <leotron13522@gmail.com>
2021-08-24 12:23:09 +0300

    bash scripts for linux

--- 4.Linux/t5_2_1_1.sh
+++ 4.Linux/t5_2_1_1.sh
@@ -13,7 +13,6 @@ do
     for (( x=0; x<deep; x++ ))
     do
         random_name=$(tr -dc A-Za-z0-9_ < /dev/urandom | head -c 8 | xargs)
-        #echo $deep
         path=$path/$random_name/ 
     done
     if (( $RANDOM%2 )) ; then
